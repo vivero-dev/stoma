786976059aee915cd02d93b33c80135b636a6f63
Jonathan Bennett <github@jldb.co.uk>
2026-04-20 14:21:57 +0200

    chore: fixing docs config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,7 +10,9 @@ import demoApiPlugin from "./vite-plugin-demo-api";
 export default defineConfig({
   site: "https://stoma.vivero.dev/",
   markdown: {
-    syntaxHighlight: "shiki",
+    shikiConfig: {
+      themes: { light: "github-light", dark: "github-dark-default" },
+    },
   },
   vite: {
     plugins: [demoApiPlugin()],
